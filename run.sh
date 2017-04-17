800bf4a162edb2aa3fb710546ed67ac82ce30dcf
Martin Breuer <martin.breuer@carjump.de>
2017-04-17 23:58:35 +0200

    Made sh script code as readable as bat code

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-if [ $# -lt 3 ]; then echo "This script requires the next parameters:"; echo "- absolute path to apk file"; echo "- filter (can be a package name or 'nofilter' string)"; echo "- true or false (where true means that you want to see inner classes on your graph)"; echo "Examples:"; echo "./run.sh full/path/to/the/apk/app-release.apk com.example.test true"; echo "./run.sh full/path/to/the/apk/app-release.apk nofilter false"; exit 1; fi
+if [ $# -lt 3 ]; then
+  echo "This script requires the next parameters:";
+  echo "- absolute path to apk file";
+  echo "- filter (can be a package name or 'nofilter' string)";
+  echo "- true or false (where true means that you want to see inner classes on your graph)";
+  echo "Examples:";
+  echo "./run.sh full/path/to/the/apk/app-release.apk com.example.test true";
+  echo "./run.sh full/path/to/the/apk/app-release.apk nofilter false";
+  exit 1;
+fi
 fileName="$1"
 xbase=${fileName##*/}
 xpref=${xbase%.*}
