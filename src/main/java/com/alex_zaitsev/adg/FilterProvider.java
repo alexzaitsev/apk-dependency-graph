d42740d1fabbd943448513a355537cfe397b7a11
LaurieScheepers <laurscheepers@gmail.com>
2020-09-06 13:33:07 +0200

    Add support for Windows. See https://github.com/alexzaitsev/apk-dependency-graph/issues/60 for an explanation. Fixed unit test.

--- src/main/java/com/alex_zaitsev/adg/FilterProvider.java
+++ src/main/java/com/alex_zaitsev/adg/FilterProvider.java
@@ -3,6 +3,8 @@ package com.alex_zaitsev.adg;
 import com.alex_zaitsev.adg.io.*;
 import com.alex_zaitsev.adg.filter.*;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import java.io.File;
 import java.util.regex.Matcher;
 
@@ -17,21 +19,17 @@ public class FilterProvider {
     public Filter<String> makePathFilter() {
         String replacement = Matcher.quoteReplacement(File.separator);
 	    replacement = Matcher.quoteReplacement(replacement);
-        String packageNameAsPath = inputFilters.getPackageName().replaceAll("\\.", replacement);
+
+	    // see https://github.com/alexzaitsev/apk-dependency-graph/issues/60
+        String packageNameAsPath = inputFilters.getPackageName().replaceAll(SystemUtils.IS_OS_WINDOWS ? "/" : "\\.", replacement);
         String packageNameRegex = ".*" + packageNameAsPath + ".*";
-        RegexFilter filter = new RegexFilter(packageNameRegex);
-        
-        return filter;
+
+        return new RegexFilter(packageNameRegex);
     }
 
     public Filter<String> makeClassFilter() {
         String[] ignoredClasses = inputFilters.getIgnoredClasses();
-        if (ignoredClasses == null) {
-            return null;
-        }
 
-        InverseRegexFilter ignoredClassesFilter = new InverseRegexFilter(ignoredClasses);
-
-        return ignoredClassesFilter;
+        return ignoredClasses == null ? null : new InverseRegexFilter(ignoredClasses);
     }
 }
